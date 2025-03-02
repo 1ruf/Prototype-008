274bfa09eb8e84e16bd695a8225499e71fa19ecd
고한결 <gyeol0411@naver.com>
2025-03-02 13:13:52 +0900

    .add VisualEffect

--- Prototype-008/Assets/Scripts/Explosion/ExplosionManager.cs
+++ Prototype-008/Assets/Scripts/Explosion/ExplosionManager.cs
@@ -1,8 +1,13 @@
 using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ExplosionManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _explosionLight;
+    [SerializeField] private ParticleSystem _explosionParticle;
+    [Header("Necessery")]
     [SerializeField] private ExplodeValueSO valueSO;
     private Vector3 _ePoint;
 
@@ -20,6 +25,7 @@ public class ExplosionManager : MonoBehaviour
 
     private void EfterEffect(Collider[] Objs)
     {
+        VisualEffect();
         foreach (Collider obj in Objs)
         {
             Rigidbody objRigidbody;
@@ -28,12 +34,25 @@ public class ExplosionManager : MonoBehaviour
             {
                 Vector3 direction = objRigidbody.position - _ePoint;
                 float distance = Vector3.Distance(objRigidbody.position, _ePoint);
-                float power = _pressure * Mathf.Pow((2/distance),2);
+                float power = _pressure * Mathf.Pow((1 / distance), 2);
 
                 objRigidbody.AddForce(direction * power,ForceMode.Impulse);
             }
             else print($"{obj.name} has no rigidbody!");
         }
+        StartCoroutine(DestroyCoroutine(_explosionLight,0.1f));
+    }
+
+    private void VisualEffect()
+    {
+        _explosionLight.SetActive(true);
+        _explosionParticle.Play();
+    }
+
+    private IEnumerator DestroyCoroutine(GameObject target,float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(target);
     }
 
     private void OnDrawGizmos()
