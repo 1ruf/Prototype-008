3fe5f290a4c3cc538252800e4665a2868d4c0eb3
고한결 <gyeol0411@naver.com>
2025-03-07 17:20:56 +0900

    .update PlrPos

--- Prototype-008/Assets/Scripts/Test/HumanTouch.cs
+++ Prototype-008/Assets/Scripts/Test/HumanTouch.cs
@@ -5,31 +5,9 @@ public class HumanTouch : MonoBehaviour
     [SerializeField] private float moveSpeed;
 
     private Rigidbody rigid;
-    private Vector3 moveDir;
     private void Awake()
     {
         rigid = GetComponent<Rigidbody>();
-    }
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            moveDir = (moveDir + Vector3.left).normalized;
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            moveDir = (moveDir + Vector3.right).normalized;
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            moveDir = (moveDir + Vector3.forward).normalized;
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            moveDir = (moveDir + Vector3.back).normalized;
-        }
-        rigid.AddForce(moveDir * moveSpeed);
-        moveDir = Vector3.zero;
     }
     private void OnCollisionEnter(Collision collision)
     {
